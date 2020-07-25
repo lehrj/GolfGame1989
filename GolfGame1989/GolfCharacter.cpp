90be38c104ccde3632897bb307aeee8280f833aa
w.lehrjackson <w.lehrjackson@gmail.com>
2020-07-24 20:54:19 -0400

    Code cleanup , remove extra character data

--- GolfGame1989/GolfCharacter.cpp
+++ GolfGame1989/GolfCharacter.cpp
@@ -23,50 +23,7 @@ void GolfCharacter::LoadCharacterData()
     m_characters[i].armMass = 7.3;
     m_characters[i].armMassMoI = 1.15;
     m_characters[i].clubLengthModifier = 1.0;
-
-    /*
-        m_characters[i].name = "Natty Noonan";
-    m_characters[i].bioLine0 = "The Natural";
-    m_characters[i].bioLine1 = "1";
-    m_characters[i].bioLine2 = "2";
-    m_characters[i].bioLine3 = "3";
-    m_characters[i].armBalancePoint = 0.5;
-    m_characters[i].armLength = 0.62;
-    m_characters[i].armMass = 7.3;
-    m_characters[i].armMassMoI = 1.15;
-    m_characters[i].clubLengthModifier = 1.0;
-    ///////////////////////////////////////
-    m_characters[i].name = "Natty Noonan";
-    m_characters[i].bio = "The Natural";
-    m_characters[i].armBalancePoint = 0.5;
-    m_characters[i].armLength = 0.82;
-    m_characters[i].armMass = 5.3;
-    m_characters[i].armMassMoI = 1.15;
-    m_characters[i].clubLengthModifier = 1.3;
-    */
     ++i;
-
-    /*
-    m_characters[i].name = "Larry Leisureson";
-    m_characters[i].bio = "Fan of Leisure suits and knows that a drink tossed in his face means no";
-    m_characters[i].armBalancePoint = 0.5;
-    m_characters[i].armLength = 0.92;
-    m_characters[i].armMass = 10.3;
-    m_characters[i].armMassMoI = 1.15;
-    m_characters[i].clubLengthModifier = 1.0;
-    //////////////////
-    m_characters[i].name = "Larry Leisureson";
-    m_characters[i].bioLine0 = "Big fan of leisure suits";
-    m_characters[i].bioLine1 = "but knows that a drink ";
-    m_characters[i].bioLine2 = "tossed in his face means no";
-    m_characters[i].bioLine3 = "3";
-    m_characters[i].armBalancePoint = 0.500069;
-    m_characters[i].armLength = 0.620069;
-    m_characters[i].armMass = 7.300069;
-    m_characters[i].armMassMoI = 1.150069;
-    m_characters[i].clubLengthModifier = 1.100069;
-
-    */
     
     m_characters[i].name = "Larry Leisureson";
     m_characters[i].bioLine0 = "Although he is a big fan of";
@@ -74,21 +31,11 @@ void GolfCharacter::LoadCharacterData()
     m_characters[i].bioLine2 = "he still knows that a drink";
     m_characters[i].bioLine3 = "tossed in his face means no";
     m_characters[i].armBalancePoint = 0.500069;
-    m_characters[i].armLength = 0.620069;
-    m_characters[i].armMass = 7.300069;
+    m_characters[i].armLength = 0.740069;
+    m_characters[i].armMass = 5.200069;
     m_characters[i].armMassMoI = 1.150069;
     m_characters[i].clubLengthModifier = 1.100069;
-    
     ++i;
-    /*
-    m_characters[i].name = "Chubs McShortRound";
-    m_characters[i].bio = "Mario ate the skinny one and then got hit by a turtle thing";
-    m_characters[i].armBalancePoint = 0.5;
-    m_characters[i].armLength = 0.62;
-    m_characters[i].armMass = 7.3;
-    m_characters[i].armMassMoI = 1.15;
-    m_characters[i].clubLengthModifier = 1.0;
-    */
 
     m_characters[i].name = "Chubs McShortRound";
     m_characters[i].bioLine0 = "An Italian plumber that ate";
@@ -96,8 +43,8 @@ void GolfCharacter::LoadCharacterData()
     m_characters[i].bioLine2 = "then got hit by some kind ";
     m_characters[i].bioLine3 = "of drunk turtle thing";
     m_characters[i].armBalancePoint = 0.500069;
-    m_characters[i].armLength = 0.620069;
-    m_characters[i].armMass = 7.300069;
+    m_characters[i].armLength = 0.510069;
+    m_characters[i].armMass = 8.600069;
     m_characters[i].armMassMoI = 1.150069;
     m_characters[i].clubLengthModifier = .900069;
 }
